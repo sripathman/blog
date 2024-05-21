fb5651efe82ba60eef8d020bda0a14480986e734
sripathman <nsripathman@gmail.com>
2024-05-21 15:37:02 +0100

    Dynamic URL segments and  Redirects

--- cs50w/urls.py
+++ cs50w/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 
 urlpatterns = [
-  path("", views.index, name= "index")
+  path("", views.index, name= "index"),
+#path("post/detail", views.detail ,name= "detail")
+  path("post/<str:post_id>", views.detail , name= "detail"),
+   path("new_url", views.new_url_view, name= "new_url"),
+   path("old_url", views.old_url_redirect, name= "old_url"),
 ]
--- cs50w/views.py
+++ cs50w/views.py
@@ -1,9 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 
 # Create your views here.
 # def index(request):
  # return HttpResponse("Hello world. you are at blog's index")from django.shortcuts import render
  
+
+# Create your views here.
 def index(request):
-  return HttpResponse("Hello world. you are at blog's index")
+  return HttpResponse ("Hello world. you are at blog's index")
+def detail(response, post_id):
+  return HttpResponse ( f"you are at detail post page and Id is {post_id}")
+def old_url_redirect(request):
+  return redirect("new_url")
+def new_url_view(request):
+    return HttpResponse("this is new url ")
