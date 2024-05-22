6fb156c1f058a8d83f1aa1d71f5baaee20b13fad
sripathman <nsripathman@gmail.com>
2024-05-22 10:15:34 +0100

    Reverse and Named URLS

--- cs50w/urls.py
+++ cs50w/urls.py
@@ -2,11 +2,11 @@ from django.urls import path ,include
 from . import views
 
 
-
+app_name = 'cs50w'
 urlpatterns = [
   path("", views.index, name= "index"),
-#path("post/detail", views.detail ,name= "detail")
+  #path("post/detail", views.detail ,name= "detail")
   path("post/<str:post_id>", views.detail , name= "detail"),
-   path("new_url", views.new_url_view, name= "new_url"),
-   path("old_url", views.old_url_redirect, name= "old_url"),
+   path("new_url", views.new_url_view, name= "new_page_url"),
+   path("old_url", views.old_url_redirect, name = "old_url"),
 ]
--- cs50w/views.py
+++ cs50w/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,reverse
 from django.http import HttpResponse
 
 # Create your views here.
@@ -12,6 +12,6 @@ def index(request):
 def detail(response, post_id):
   return HttpResponse ( f"you are at detail post page and Id is {post_id}")
 def old_url_redirect(request):
-  return redirect("new_url")
+  return redirect(reverse('cs50w:new_page_url'))
 def new_url_view(request):
     return HttpResponse("this is new url ")
