5e0f146e1fb9b464969a4dbe3078d06b399a2c2e
sripathman <nsripathman@gmail.com>
2024-05-27 07:11:28 +0100

    Returning HTML

--- cs50w/apps.py
+++ cs50w/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class Css50WConfig(AppConfig):
+class Cs50WConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'css50w'
+    name = 'cs50w'
--- cs50w/views.py
+++ cs50w/views.py
@@ -1,17 +1,14 @@
-from django.shortcuts import render,redirect,reverse
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
-# Create your views here.
-# def index(request):
- # return HttpResponse("Hello world. you are at blog's index")from django.shortcuts import render
- 
 
 # Create your views here.
 def index(request):
-  return HttpResponse ("Hello world. you are at blog's index")
-def detail(response, post_id):
-  return HttpResponse ( f"you are at detail post page and Id is {post_id}")
+  return render(request,"blog/index.html")
+def detail(request, post_id):
+    return render(request, "blog/detail.html")
+ # return HttpResponse ( f"you are at detail post page and Id is {post_id}")
 def old_url_redirect(request):
-  return redirect(reverse('cs50w:new_page_url'))
+  return redirect("new_url")
 def new_url_view(request):
     return HttpResponse("this is new url ")
